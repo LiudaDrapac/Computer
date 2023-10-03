df4be154070c1cb071a9e43d54c036490e2e1175
Ludmila <ludmila.drapac@cnas.gov.md>
2023-10-02 23:27:39 -0800

    Initial commit

---
+++ src/edu/tekwill/academy/computer/Computer.java
@@ -0,0 +1,22 @@
+package edu.tekwill.academy.computer;
+
+import edu.tekwill.academy.computer.components.HardDrive;
+import edu.tekwill.academy.computer.components.Motherboard;
+import edu.tekwill.academy.computer.components.Processor;
+import edu.tekwill.academy.computer.peripherals.Keyboard;
+import edu.tekwill.academy.computer.peripherals.Monitor;
+import edu.tekwill.academy.computer.peripherals.Mouse;
+
+public class Computer {
+    Processor processor;
+
+    Motherboard motherboard;
+
+    HardDrive hardDrive;
+
+    Monitor monitor;
+
+    Mouse mouse;
+
+    Keyboard keyboard;
+}
